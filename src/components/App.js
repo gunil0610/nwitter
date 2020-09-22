045822ab56fc18939569ae03500a03542a2ff923
Gunil <gunil.na1@gmail.com>
2020-09-22 18:47:58 +0200

    cleaned userobj

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,11 @@ function App() {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       } else {
         setIsLoggedIn(false);
       }
@@ -20,7 +24,12 @@ function App() {
   }, []);
 
   const refreshUser = () => {
-    setUserObj(authService.currentUser);
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
   };
 
   return (
