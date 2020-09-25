3d849e68d47b483fb9c15ceb2cdef4cf8a2dbb5c
Gunil <gunil.na1@gmail.com>
2020-09-25 18:20:40 +0200

    Updates

---
+++ precache-manifest.2ed0c42d3be3b20af3b1cc1910439593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9bc54b5c637268b9592276e42be0a9",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "ee5ad4d7727c0b8cc1f3",
+    "url": "/nwitter/static/js/2.97abf4cb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.97abf4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c7cc0edb6f4eeeaee7",
+    "url": "/nwitter/static/js/main.cf41578e.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
